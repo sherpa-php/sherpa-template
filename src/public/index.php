534fdd82898afd6e36ae4bfec7e6ad2d7b856d13
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-18 01:49:42 +0200

    feat: import `vendor/autoload` and add some imports

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,15 @@
 <?php
 
+use Dotenv\Dotenv;
+use Sherpa\Core\router\Router;
+use Sherpa\Core\utilities\toolbar\Toolbar;
+use Sherpa\Template\routes\core\RouterLoader;
+
+$startTime = microtime(true);
+
 session_start();
 
 const ROOT = "../..";
+
+
+require_once ROOT . "/vendor/autoload.php";
