a52124db30617fb0127b12b81a178bebe23e971e
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-18 01:47:51 +0200

    feat: create `RouterLoader` utility class

---
+++ src/routes/core/RouterLoader.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Sherpa\Template\routes\core;
+
+use Sherpa\Core\router\Route;
+
+class RouterLoader
+{
+    /** Routes directory path from web root. */
+    private const ROUTES_DIRECTORY = "../routes/";
+
+    /**
+     * @return array Routes repositories filename list
+     */
+    public static function getRoutesRepositories(): array
+    {
+        $scan = scandir(self::ROUTES_DIRECTORY);
+
+        return array_filter($scan, function ($element)
+        {
+            return is_file(self::ROUTES_DIRECTORY . "/$element")
+                && pathinfo($element, PATHINFO_EXTENSION) === "php";
+        });
+    }
+
+    /**
+     * Find routes repository by given filename and open it.
+     *
+     * @param string $name
+     */
+    private static function prepareRoutesRepository(string $name): void
+    {
+        require_once self::ROUTES_DIRECTORY . "/$name";
+    }
+
+    /**
+     * Find all routes repositories and open each one.
+     */
+    public static function prepareRoutesRepositories(): void
+    {
+        foreach (self::getRoutesRepositories() as $repository)
+        {
+            self::prepareRoutesRepository($repository);
+        }
+    }
+}
