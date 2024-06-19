b9c6ce4e1ad5926edebbc1ba40e26de39e6742a6
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-19 02:50:56 +0200

    feat: create `phpViewFunctions.php` utility functions file
    with first function: `component` to use component in PHP views

---
+++ src/core/phpViewFunctions.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * PHP view rendering system utilities functions.
+ */
+
+
+use Sherpa\Template\core\View;
+
+function component(string $name, array $props = []): void
+{
+    if (View::isPhpComponentExisting($name))
+    {
+        extract($props);
+
+        include "../views/components/$name.component.php";
+    }
+}
