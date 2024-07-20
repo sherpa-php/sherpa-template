fdee90a433bc846e66bedf06a76c32394ce23b46
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-20 21:10:01 +0200

    feat: implement database process to index file

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Dotenv\Dotenv;
+use Sherpa\Core\database\DB;
 use Sherpa\Core\exceptions\ExceptionsManager;
 use Sherpa\Core\router\Router;
 use Sherpa\Core\utilities\toolbar\Toolbar;
@@ -24,6 +25,15 @@ $env->load();
 
 
 ExceptionsManager::useExceptionHandler();
+
+
+$database = DB::connect(
+    $_ENV["DB_DBMS"],
+    $_ENV["DB_HOST"],
+    $_ENV["DB_PORT"],
+    $_ENV["DB_DATABASE"],
+    $_ENV["DB_USER"],
+    $_ENV["DB_PASSWORD"]);
 
 
 RouterLoader::prepareRoutesRepositories();
