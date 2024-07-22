e3f6013aac4cc763ca20eaffe0bb08897a7b1945
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-22 22:03:54 +0200

    feat: create middlewares register file

---
+++ src/middlewares/register.php
@@ -0,0 +1,18 @@
+<?php
+
+use Sherpa\Core\middlewares\CSRFTokenMiddleware;
+
+
+/*
+ * Middlewares register
+ * --
+ *
+ * This array contains all middleware to enable by register.
+ */
+
+
+return [
+
+    // Register your middlewares here!
+
+];
